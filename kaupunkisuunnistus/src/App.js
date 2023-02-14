2b65f9ad0dd86a2f8aa120215a0bbe2a19df862d
EmBBuU <emppu92@windowslive.com>
2023-02-14 23:03:26 +0200

    empun toinen testi

--- kaupunkisuunnistus/src/App.js
+++ kaupunkisuunnistus/src/App.js
@@ -5,7 +5,7 @@ const App = () => (
     <p>Tarvitaan tuutoreja ;_;</p>
     <p>testiiiiii</p>
     <p>dev_atte initial test push</p>
-    <p>EmBBu testaaa</p>
+    <p>EmBBu testaaa1</p>
   </div>
 );
 

9cbbafb22d881b0ff245cde27e21c8e5064b6e8b
EmBBuU <emppu92@windowslive.com>
2023-02-21 23:14:44 +0200

    Taulukot by emppu

--- kaupunkisuunnistus/src/App.js
+++ kaupunkisuunnistus/src/App.js
@@ -1,9 +1,12 @@
-import React from "react"
+import React from "react";
 import Button from "./components/Button";
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import InfoPage from "./components/InfoPage"
-import Homepage from "./components/Homepage"
+import InfoPage from "./components/InfoPage";
+import Homepage from "./components/Homepage";
+import ScoreTable from "./components/ScoreTable";
+import CheckpointTable from "./components/CheckpointTable";
+import "./Style.css";
 
 //Julian Kommentti: tällähetkellä homepage-linkki toimii, eli ylhäällä olevaa otsikkoa klikatessa routataan homepage-komponenttiin
 //en vielä ole varma miten ton buttonin voi yhdistää infopage komponenttiin, mut kattelen sitä myöhemmin/joku muu voi katella
@@ -11,16 +14,21 @@ import Homepage from "./components/Homepage"
 
 const App = () => (
   <div>
-    <Navbar/>
-
-
+    <Navbar />
     <div className="routeContainer">
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/InfoPage" element={<InfoPage />} />
       </Routes>
     </div>
-
+    <div>
+      <h1>RASTIT</h1>
+      <CheckpointTable />
+    </div>
+    <div>
+      <h1>PISTETILANNE</h1>
+      <ScoreTable />
+    </div>
   </div>
 );
 

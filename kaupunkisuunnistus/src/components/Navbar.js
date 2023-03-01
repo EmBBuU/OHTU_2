fb5a838239c2e4d31baa87efcbe429be7fb5e4b9
Julia <juliajun@uef.fi>
2023-03-01 22:57:22 +0200

    Navbar vähän sievemmäksi

--- kaupunkisuunnistus/src/components/Navbar.js
+++ kaupunkisuunnistus/src/components/Navbar.js
@@ -3,30 +3,35 @@ import logo from '../images/logo.png'
 
 //täällä vielä olematon linkki tuo "Kirjaudu sisään", eli se ei
 //johda atm mihinkää ku tota sivua ei oo olemassa viel
-//ja tosiaan tän ulkonäöllinen muotoilu on kesken, en tiiä miten sen sais sillai tosi cooliksi
 
 import React from 'react'
 
 function Navbar() {
-  return (
-    <div>
-        <nav className="nav">
-            <img src={logo} alt="logo" className="logo"/>
-            <Link to="/" className="heading">ATK-YTP JOENSUU</Link>
+    return (
+        <div>
+            <table className="nav">
+                <tbody>
+                    <tr>
+                        <td className="navTop">
+                            <img src={logo} alt="logo" className="logo" />
+                            <Link to="/" className="heading">ATK-YTP JOENSUU</Link>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="navBottom">
+                            <button className='btn-login'>
+                                <Link to="/KirjauduSisaan" className="login">KIRJAUDU SISÄÄN</Link>
+                            </button>
 
-            <div className="navButtons">
-                <button className='btn-login'>
-                    <Link to="/KirjauduSisaan" className="login">KIRJAUDU SISÄÄN</Link>
-                </button>
-
-                <button className='btn-info'>
-                    <Link to="/InfoPage" className="infoPage">Ohjeet</Link>
-                </button>
-            </div>
-
-        </nav>
-    </div>
-  )
+                            <button className='btn-info'>
+                                <Link to="/InfoPage" className="infoPage">Ohjeet</Link>
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default Navbar

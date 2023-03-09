0966c3cc0845d0ef9d04fde21ad6b446416d16ac
Jussi <jussi.kukko@gmail.com>
2023-03-09 23:37:57 +0200

    Backend: MongoDB cluster done with few populations

--- kaupunkisuunnistus-backend/index.js
+++ kaupunkisuunnistus-backend/index.js
@@ -1,24 +1,44 @@
-require("dotenv").config();
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
+const morgan = require('morgan');
+require('dotenv').config();
 
-app.use(express.json());
+const Team = require('./models/team')
+const Location = require('./models/location')
+
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+}
+
+const unknownEndpoint = (request, response) => {
+  console.log('unknown endpoint')
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
 app.use(cors());
+app.use(express.json());
+app.use(requestLogger);
+app.use(morgan('tiny'))
 
+/*
 let teams = [
   {
-    id: 1,
+    team_id: 1,
     name: "Oonafanit",
     score: 5,
   },
   {
-    id: 2,
+    team_id: 2,
     name: "Lampaat",
     score: 12,
   },
   {
-    id: 3,
+    team_id: 3,
     name: "Team fuksit",
     score: 3,
   },
@@ -26,41 +46,47 @@ let teams = [
 
 let locations = [
   {
-    id: 1,
+    location_id: 1,
     name: "Tiedepuiston kenttä",
   },
   {
-    id: 2,
+    location_id: 2,
     name: "Torisusi",
   },
   {
-    id: 3,
+    location_id: 3,
     name: "Suvantosilta",
   },
   {
-    id: 4,
+    location_id: 4,
     name: "Kirkkopuisto",
   },
   {
-    id: 5,
+    location_id: 5,
     name: "Carelian etupiha",
   },
 ];
+*/
 
 app.get("/", (request, response) => {
+  console.log(`Haloust heloust`)
   response.send("<h1>Haloust heloust!</h1>");
 });
 
 // Joukkueet (teams)
 app.get("/api/teams", (request, response) => {
-  response.json(teams);
+  Team.find({}).then(teams => {
+    console.log(`List of teams: `, teams)
+    response.json(teams);
+  })
 });
 
 app.get("/api/teams/:id", (request, response) => {
   const id = Number(request.params.id);
-  const team = teams.find((team) => team.id === id);
+  const team = teams.find((team) => team.team_id === id);
 
   if (team) {
+    console.log(`Details of team ${id}: `, team)
     response.json(team);
   } else {
     response.status(404).end();
@@ -69,32 +95,37 @@ app.get("/api/teams/:id", (request, response) => {
 
 app.delete("/api/teams/:id", (request, response) => {
   const id = Number(request.params.id);
-  teams = teams.filter((team) => team.id !== id);
+  teams = teams.filter((team) => team.team_id !== id);
 
   response.status(204).end();
 });
 
 app.post("/api/teams", (request, response) => {
-  const maxId = teams.length > 0 ? Math.max(...teams.map((t) => t.id)) : 0;
+  const maxId = teams.length > 0 ? Math.max(...teams.map((t) => t.team_id)) : 0;
 
   const team = request.body;
-  team.id = maxId + 1;
+  team.team_id = maxId + 1;
 
   teams = teams.concat(team);
+  console.log(`Added team: `, team)
 
   response.json(team);
 });
 
 // Rastipaikat (location)
 app.get("/api/locations", (request, response) => {
-  response.json(locations);
+  Location.find({}).then(locations => {
+    console.log(`List of locations: `, locations)
+    response.json(locations);
+  })
 });
 
 app.get("/api/locations/:id", (request, response) => {
   const id = Number(request.params.id);
-  const location = locations.find((location) => location.id === id);
+  const location = locations.find((l) => l.location_id === id);
 
   if (location) {
+    console.log(`Details of location ${id}: `, location)
     response.json(location);
   } else {
     response.status(404).end();
@@ -103,39 +134,27 @@ app.get("/api/locations/:id", (request, response) => {
 
 app.delete("/api/locations/:id", (request, response) => {
   const id = Number(request.params.id);
-  locations = locations.filter((location) => location.id !== id);
+  locations = locations.filter((l) => l.location_id !== id);
 
   response.status(204).end();
 });
 
 app.post("/api/locations", (request, response) => {
   const maxId =
-    locations.length > 0 ? Math.max(...locations.map((l) => l.id)) : 0;
+    locations.length > 0 ? Math.max(...locations.map((l) => l.location_id)) : 0;
 
   const location = request.body;
-  location.id = maxId + 1;
+  location.location_id = maxId + 1;
 
   locations = locations.concat(location);
+  console.log(`Added location: `, location)
 
   response.json(location);
 });
 
-const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method);
-  console.log("Path:  ", request.path);
-  console.log("Body:  ", request.body);
-  console.log("---");
-  next();
-};
-
-app.use(requestLogger);
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
-
 app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3002;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

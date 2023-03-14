622d0d2f58889e05a778309d2e726b4bdb7d928e
EmBBuU <emppu92@windowslive.com>
2023-03-14 11:28:51 +0200

    dropdown ja uudet sivut

--- kaupunkisuunnistus/src/components/GivePoints.js
+++ kaupunkisuunnistus/src/components/GivePoints.js
@@ -1,17 +1,37 @@
 import React from "react";
-import ScoreTable from "./ScoreTable";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-function GivePoints() {
+const GivePoints = () => {
+  const [teams, setTeams] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3002/api/teams").then((response) => {
+      setTeams(response.data);
+    });
+  }, []);
+
   return (
-    <div>
-      <div>
-        <h1>RASTI NRO X</h1>
-      </div>
-      <div>
-        <ScoreTable />
-      </div>
+    // useRef -> Minkä rastin pisteitä ollaan antamassa!!
+    <div className="ScoreTable">
+      <table>
+        <tr>
+          <th>RYHMÄN NIMI</th>
+          <th>RYHMÄN PISTEET RASTILTA</th>
+        </tr>
+        {teams
+          // Tähän kohtaan kaipaisi .sort -metodia, että saataisiin joukkueet lajiteltua pisteiden mukaan.
+          .map((team, key) => {
+            return (
+              <tr key={key}>
+                <td>{team.name}</td>
+                <td>{team.points}</td>
+              </tr>
+              //Tuohon team.point -muuttujaan menis ne napeilla annetut pisteet!!!
+            );
+          })}
+      </table>
     </div>
   );
-}
-
+};
 export default GivePoints;

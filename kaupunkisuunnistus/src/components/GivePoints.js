4430e48818365589c2c2be228ca592a0e3231449
Sanni Oksanen <sannioks@uef.fi>
2023-03-15 10:53:31 +0200

    plus ja miinus napit

--- kaupunkisuunnistus/src/components/GivePoints.js
+++ kaupunkisuunnistus/src/components/GivePoints.js
@@ -1,9 +1,22 @@
 import React from "react";
+import Button from "./Button";
 import ScoreTable from "./ScoreTable";
 
-function GivePoints() {
+
+const GivePoints = () =>{
+  const [ counter, setCounter ] = React.useState(0)
+
   return (
     <div>
+    <div>{counter}</div>
+    <div>
+      <button onClick={() => setCounter(counter + 1)}>
+      +
+      </button>
+      <button onClick={() => setCounter(counter-1)}> 
+      -
+      </button>
+      </div>
       <div>
         <h1>RASTI NRO X</h1>
       </div>
@@ -13,5 +26,6 @@ function GivePoints() {
     </div>
   );
 }
+
 
 export default GivePoints;

fefd13545906c358e23e71e09eb8c8c532d0f269
EmBBuU <emppu92@windowslive.com>
2023-03-20 18:52:16 +0200

    säätöä vielä kerran...

--- kaupunkisuunnistus/src/components/GivePoints.js
+++ kaupunkisuunnistus/src/components/GivePoints.js
@@ -1,7 +1,6 @@
 import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import "./Counters";
 
 const GivePoints = () => {
   const [teams, setTeams] = useState([]);

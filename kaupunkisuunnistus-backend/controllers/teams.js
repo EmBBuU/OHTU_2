e4cafc9316702700bddf7897b4346a8460297d2e
EmBBuU <emppu92@windowslive.com>
2023-04-04 21:22:11 +0300

    pientä säätöä!

--- kaupunkisuunnistus-backend/controllers/teams.js
+++ kaupunkisuunnistus-backend/controllers/teams.js
@@ -1,62 +1,64 @@
-const teamsRouter = require('express').Router()
-const Team = require('../models/team')
+const teamsRouter = require("express").Router();
+const Team = require("../models/team");
 
-teamsRouter.get('/', async (request, response) => {
-  const teams = await Team.find({})
-  
-  console.log('List of teams: ', teams)
+teamsRouter.get("/", async (request, response) => {
+  const teams = await Team.find({});
+
+  console.log("List of teams: ", teams);
   response.json(teams);
-})
+});
 
-teamsRouter.get('/:id', async (request, response) => {
-  const team = await Team.findById(request.params.id)
-  
+teamsRouter.get("/:id", async (request, response) => {
+  const team = await Team.findById(request.params.id);
+
   if (team) {
-    console.log(team)
-    response.json(team)
+    console.log(team);
+    response.json(team);
+  } else {
+    response.status(201).json(savedTeam);
   }
-  else {
-    response.status(201).json(savedTeam)
-  }
-})
+});
 
-teamsRouter.post('/', async (request, response) => {
-  const body = request.body
+teamsRouter.post("/", async (request, response) => {
+  const body = request.body;
 
   if (body.name === undefined) {
-    return response.status(400).json({ error: 'team name missing' })
+    return response.status(400).json({ error: "team name missing" });
   }
 
   const team = new Team({
     // team_id automatisoitu incrementti tähän?
     name: body.name,
-    score: body.score || 0
-  })
+    score: body.score || 0,
+  });
 
-  const savedTeam = await team.save()
-  response.json(savedTeam)
+  const savedTeam = await team.save();
+  response.json(savedTeam);
 });
 
 teamsRouter.delete("/:id", async (request, response) => {
-  await Team.findByIdAndRemove(request.params.id)
-  response.status(204).end()
+  await Team.findByIdAndRemove(request.params.id);
+  response.status(204).end();
 });
 
-// Ei testattu, 
-teamsRouter.put('/:id', (request, response, next) => {
-  const body = request.body
+// Ei testattu,
+teamsRouter.put("/:id", (request, response, next) => {
+  const body = request.body;
+  console.log(body.score, "asdf");
+  //undefined asdf
+  //Plan executor error during findAndModify :: caused by :: Performing an update on the path '_id' would modify the immutable field '_id'
 
   const team = new Team({
     // team_id pitää tälle tehdä jotain?
     name: body.name,
     score: body.score,
-  })
+  });
 
   Team.findByIdAndUpdate(request.params.id, team, { new: true })
-    .then(updatedTeam => {
-      response.json(updatedTeam)
+    .then((updatedTeam) => {
+      response.json(updatedTeam);
     })
-    .catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
 
-module.exports = teamsRouter
+module.exports = teamsRouter;

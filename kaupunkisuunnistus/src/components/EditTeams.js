19fd15c1bf7492489bb2ce7d4b1f041838d2e3c8
EmBBuU <emppu92@windowslive.com>
2023-04-15 16:41:20 +0300

    Tiimien nimen muokkaaminen

--- kaupunkisuunnistus/src/components/EditTeams.js
+++ kaupunkisuunnistus/src/components/EditTeams.js
@@ -5,14 +5,107 @@ import axios from "axios";
 
 const EditTeams = () => {
   const [teams, setTeams] = useState([]);
-  const [newName, setNewName] = useState([]);
+  const [editedName, setEditedName] = useState("");
+  const [editedId, setEditedId] = useState("");
 
   useEffect(() => {
     axios.get("http://localhost:3002/api/teams").then((response) => {
       setTeams(response.data);
     });
   }, []);
+
+  const updateName = (id) => {
+    try {
+      alert("Nimi on tallennettu!");
+
+      axios
+        .put(`http://localhost:3002/api/teams/${id}`, { name: editedName })
+        .then((response) => {
+          console.log(response.data);
+          setTeams((prevTeams) =>
+            prevTeams.map((team) =>
+              team._id === id ? { ...team, name: editedName } : team
+            )
+          );
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleEditName = (name, id) => {
+    setEditedName(name);
+    setEditedId(id);
+  };
+
   /*
+  const handleNameChange = (teamId, newName) => {
+    setTeams((prevState) =>
+      prevState.map((team) =>
+        team._id === teamId ? { ...team, name: newName } : team
+      )
+    );
+  };
+  */
+
+  return (
+    <div className="editteams">
+      <table>
+        <tbody>
+          <tr>
+            <th>RYHMÄN NIMI</th>
+            <th>RYHMÄN UUSI NIMI</th>
+            <th></th>
+          </tr>
+          {teams.map((team, key) => {
+            return (
+              <tr key={key}>
+                <td>{team.name}</td>
+                <td>
+                  {editedId === team._id ? (
+                    <input
+                      className="newTeamName"
+                      type="String"
+                      value={editedName}
+                      onChange={(e) => {
+                        setEditedName(e.target.value);
+                      }}
+                    />
+                  ) : (
+                    <span>{team.name}</span>
+                  )}
+                </td>
+                <td>
+                  {editedId === team._id ? (
+                    <button
+                      className="btnSaveTeam"
+                      onClick={() => updateName(team._id)}
+                    >
+                      OK
+                    </button>
+                  ) : (
+                    <button
+                      className="btnEditTeam"
+                      onClick={() => handleEditName(team.name, team._id)}
+                    >
+                      MUOKKAA
+                    </button>
+                  )}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <button className="previous">
+        <a href="/login">TAKAISIN</a>
+      </button>
+    </div>
+  );
+};
+
+export default EditTeams;
+/*
   const handleSave = (newName, teamName) => {
     console.log("OK button clicked!");
     axios
@@ -25,64 +118,3 @@ const EditTeams = () => {
       });
   };
   */
-
-  async function submit(e, oldName) {
-    e.preventDefault();
-
-    try {
-      alert("Nimi on tallennettu!");
-
-      await axios
-        .put(`http://localhost:3002/api/teams/${oldName}`, newName)
-        .then((response) => {
-          console.log(response.data);
-        });
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  return (
-    <div className="editteams">
-      <table>
-        <tbody>
-          <tr>
-            <th>RYHMÄN NUMERO</th>
-            <th>RYHMÄN NIMI</th>
-            <th>RYHMÄN UUSI NIMI</th>
-          </tr>
-          {teams
-            .sort((a, b) => b.score - a.score)
-            .map((team, key) => {
-              return (
-                <tr key={key}>
-                  <td>{key + 1}</td>
-                  <td>{team.name}</td>
-                  <td>
-                    <input
-                      className="newTeamName"
-                      type="String"
-                      onChange={(e) => {
-                        setNewName(e.target.value);
-                      }}
-                    />
-                    <button
-                      className="btnSaveTeam"
-                      onClick={(submit, team.name)}
-                    >
-                      OK
-                    </button>
-                  </td>
-                </tr>
-              );
-            })}
-        </tbody>
-      </table>
-      <button className="previous">
-        <a href="/login">TAKAISIN</a>
-      </button>
-    </div>
-  );
-};
-
-export default EditTeams;
